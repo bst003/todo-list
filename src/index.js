8722e7606655ecf94d46aefe02ff84687590278d
bst003 <bstanner33@yahoo.com>
2022-05-31 23:56:50 -0400

    Adding general notes to project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import './assets/scss/styles.scss';
 
+/*
+
+General notes
+    Use object assign for general functions like deleting item (project or todo)?
+    How to implement pub/sub pattern
+
+Modules Needed
+    Dom Stuff
+    ToDos/Tasks
+    Project/Category
+    General Shared Functions
+
+
+Todos
+    Data
+        title
+        description
+        dueData
+        priority
+    
+
+Projects
+    Data
+        title
+        description
+    Default project that all todos are in
+    What happens to todos in project if project is deleted?
+    
+
+*/
 
 console.log('hello all');
