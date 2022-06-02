9a660a4f85fa374b4de8143883ace0414039858a
bst003 <bstanner33@yahoo.com>
2022-06-01 22:09:23 -0400

    Create modules needed for project

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import './assets/scss/styles.scss';
 General notes
     Use object assign for general functions like deleting item (project or todo)?
     How to implement pub/sub pattern
+    Filter array method for clicking on projects to sort?
+    Add default category on load?
+
 
 Modules Needed
     Dom Stuff
     ToDos/Tasks
     Project/Category
     General Shared Functions
+    PubSub
 
 
 Todos
@@ -19,6 +23,9 @@ Todos
         description
         dueData
         priority
+    Able to mark complete
+    Able to delete
+    Able to edit
     
 
 Projects
@@ -27,6 +34,9 @@ Projects
         description
     Default project that all todos are in
     What happens to todos in project if project is deleted?
+        DELETE THEM
+    Able to delete
+    Able to edit
     
 
 */
