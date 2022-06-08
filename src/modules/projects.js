fc8cc51876ef6ba7e209b16102691991833209c2
bst003 <bstanner33@yahoo.com>
2022-06-07 23:24:41 -0400

    Start working on projects based mthods

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,13 +1,44 @@
 export const projectFunctions = (() => {
 
+    // Factories
+
+    const factory = ( title) => {
+
+        const data = {
+            "title": title,
+        }
+
+        return Object.assign(
+            {},
+            generalFunctions.titleMethods(data)
+        )
+
+    }
+
     
     // Private variables/functions
 
 
     // Public variables/functions
+    const projects = [];
+
+    const addProject = (object) => {
+        
+        projects.push(object);
+
+        const data = {
+            object: object,
+            array: projects
+        }
+
+        pubsub.publish('projectAdded', data );
+    }
 
 
     return {
+        factory,
+        projects,
+        addProject
     }
 
 })();
